7d3d4e5d420e23aae8a207e87e0988f1d95ae03f
mcthomas <mcthomas4@wisc.edu>
2020-11-29 00:48:23 -0600

    Added working files

---
+++ Fragment.js
@@ -0,0 +1,64 @@
+precision highp float;
+//value unchanging for scene unless by host program, accessible by both v/s and f/s
+uniform float time;
+//position vector accessible between v/s and f/s
+varying vec3 fPosition;
+//normalized vector accessible between v/s and f/s
+varying vec3 fNormal;
+
+varying vec3 fRawX;
+
+
+//determines shader ambient light ratio (light from all directions)
+const float ambient_coeff   = 0.20;
+//determines shader specular light ratio (reflection point light)
+const float specular_coeff  =0.10;
+//determines shader specular light exponent multiplier
+const float specular_exp    =2.0;
+//determines light direction in world coords
+const vec3  light_direction = vec3(-1.0,1.0,1.0); // stationary light
+//rgb vector specifying light's normalized values for color proportions
+const vec3  light_color     = vec3(0.5,0.5,0.5); // grey
+//rgb vector specifying objects' normalized values for color proportions
+const vec3  object_color    = vec3(0.5,0.5,0.5); // grey
+
+//entry point
+void main()
+{
+  //indicates change in time as a product of time and a float 50.0 multiplier
+  float delta = time*20.0;
+  //direction towards light
+  vec3 l = normalize(light_direction);
+  //normized vector perpendicular to surface
+  vec3 n = normalize(fNormal);
+  //vector pointing towards camera
+  vec3 e = normalize(-fPosition);
+  //halfway vector used to calculate light intensity into camera
+  vec3 h = normalize (e+l);
+
+  //fractioned rgb values derived from object color vertex
+  vec3 ambient_color  = ambient_coeff  * object_color;
+  //calculates specular color based upon coeff var and power of dot product of surface's normal vector and halfway vector, with lower bound of 0.0, as a product of the light's color vector
+  vec3 specular_color = specular_coeff * pow(max(0.0,dot(n,h)),specular_exp) * light_color;
+
+  //R3 vectors defining separate coordinate trigonomic evalutations for time variance; creates a continuous rotating color distribution (best seen on "Cube" from isomatric angle)
+  vec3 xy = vec3(cos(delta),0,sin(delta));
+  vec3 yx = vec3(sin(delta),0,cos(delta));
+  vec3 ydivx = vec3(cos(delta),0,tan(delta));
+  
+  //diffuse (light varying towards source) specification vars for three trigonomic subdividions to range 2
+  float redDiffuse = pow(dot(fNormal,xy),0.666);
+  float greenDiffuse = pow(dot(fNormal,yx),1.333);
+  float blueDiffuse = pow(dot(fNormal,ydivx),2.0);
+  
+  vec3 redCol = redDiffuse * vec3(1,0,0);
+  vec3 greenCol = greenDiffuse * vec3(0,1,0);
+  vec3 blueCol = blueDiffuse * vec3(0,0,1);
+  
+  if(sin(300.0*fRawX.x)>0.0) discard;
+  if(cos(300.0*fRawX.y)>0.0) discard;
+
+  
+    //specifies fragment color applied to vertices' triangles
+  gl_FragColor = vec4(ambient_color+specular_color+redCol + greenCol + blueCol, 0.5);
+}
---
+++ Vertex.js
@@ -0,0 +1,56 @@
+precision highp float;
+//position vector in object space
+attribute vec3 position;
+//normalized vector in object space
+attribute vec3 normal;
+//normalized matrix accessible between v/s and f/s
+uniform mat3 normalMatrix;
+//model view matrix accessible between v/s and f/s
+uniform mat4 modelViewMatrix;
+//projection matrix accessible between v/s and f/s
+uniform mat4 projectionMatrix;
+//normalized vector accessible between v/s and f/s
+varying vec3 fNormal;
+//position vector accessible between v/s and f/s
+varying vec3 fPosition;
+//value unchanging for scene unless by host program, accessible by both v/s and f/s
+uniform float time;
+
+//model-view coordinates accesible between v/s and f/s
+varying vec3 fRawX;
+
+const float pi=3.14159;
+varying vec3 modelX;
+varying vec3 modelN;
+
+vec2 Rotate2D(vec2 vec_in, float angle)
+{
+  vec2 vec_out;
+  vec_out.x=cos(angle)*vec_in.x-sin(angle)*vec_in.y;
+  vec_out.y=sin(angle)*vec_in.x+cos(angle)*vec_in.y;
+  return vec_out;
+}
+
+//entry point
+void main()
+{
+  
+  modelX=position;
+  modelN=normal;
+  
+  // Comment these lines out to stop twisting
+  modelX.xz = Rotate2D(modelX.xz,0.5*pi*modelX.y*sin(10.0*time)); // Try commenting out *just* this line :)
+  modelN.xz = Rotate2D(modelN.xz,0.5*pi*modelX.y*sin(10.0*time)); // This is simple as that only since the transform is rotation
+  
+  //calculates normalized varying of the product of the uniform normalized matrix and the normalized vector attribute
+  fNormal = normalize(normalMatrix * normal);
+  //position vector as a product of the uniform model view matrix and a vector comprising the position attribute and a float
+  vec4 pos = modelViewMatrix * vec4(position, 0.5);
+  //position vector varying including the three coordinates of the pos vector
+  fPosition = pos.xyz;
+  //sets the normalized device coordinates to be a product of the uniform projection matrix and the position vector
+  gl_Position = projectionMatrix * pos;
+  //sets fRawX vertex extries to modelViewMatrix coords
+  fRawX=fPosition;
+
+}
